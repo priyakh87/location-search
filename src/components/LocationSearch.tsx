194a03eab4466f3b3a6980f187b88bdcb411a33c
priya khanna <priyakh87@gmail.com>
2025-04-19 05:10:51 +0400

    search input UI changes

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -20,9 +20,9 @@ function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <label className='font-bold' htmlFor='term'>
-          Search{" "}
+         {" "}
         </label>
         <input
           className='border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full'
@@ -30,7 +30,23 @@ function LocationSearch({ onPlaceClick }: LocationSearchProps) {
           value={term}
           onChange={(e) => setTerm(e.target.value)}
         />
-      </form>
+          </form> */}
+          
+<form className="max-w-md mx-auto" onSubmit={handleSubmit}>   
+    <label htmlFor="term" className="mb-2 text-sm font-medium text-gray-900 sr-only light:text-white">Search</label>
+    <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 light:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input id='term'
+          value={term}
+          onChange={(e) => setTerm(e.target.value)} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="Search..." required />
+        {/* <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 light:bg-blue-600 light:hover:bg-blue-700 light:focus:ring-blue-800">Search</button> */}
+    </div>
+</form>
+
       <h1 className='font-bold mt-6'>Found Locations</h1>
       <div className='grid grid-cols-[1fr,40px] gap-2 mt-2 items-center'>
         {places.map((place) => {
